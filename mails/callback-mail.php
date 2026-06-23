3b105e4014c1668e3badd3e3b24d070051d3446a
Roman Vasilyev <vasilyev-r@mail.ru>
2026-06-23 13:56:05 +0300

    Del my mails address

--- mails/callback-mail.php
+++ mails/callback-mail.php
@@ -3,7 +3,7 @@
 	$win = "true";
 	$name = $_POST['name'];
 	$tel = $_POST['tel'];	
-	mail( "honey.ryazan@yandex.ru, vasilyev-r@mail.ru", "Заказ обратного звонка с сайта медовые-подарки.рф.", "Потенциальный клиент ".$name." просит перезвонить Вас на номер " . $tel );
+	mail( "honey.ryazan@yandex.ru", "Заказ обратного звонка с сайта медовые-подарки.рф.", "Потенциальный клиент ".$name." просит перезвонить Вас на номер " . $tel );
 	$_SESSION['win'] = 1;
 	$_SESSION['recaptcha'] = '<p class="text-light">Спасибо, что Вы обратились именно к нам. Мы свяжемся с Вами в ближайшее время.</p>';
 	header("Location: ".$_SERVER['HTTP_REFERER']);
--- mails/download-mail.php
+++ mails/download-mail.php
@@ -6,7 +6,7 @@ $tel = $_POST['tel'];
 $email = isset($_POST['email']) ? $_POST['email'] : 'Не указан';
 
 // Отправляем письма
-mail("honey.ryazan@yandex.ru, vasilyev-r@mail.ru", "Скачивание файла с сайта медовые-подарки.рф.", "Клиент ".$name." скачал файл. Телефон: " . $tel . ", Email: " . $email);
+mail("honey.ryazan@yandex.ru", "Скачивание файла с сайта медовые-подарки.рф.", "Клиент ".$name." скачал файл. Телефон: " . $tel . ", Email: " . $email);
 
 $_SESSION['win'] = 1;
 $_SESSION['recaptcha'] = '<p class="text-light">Скачивание начнется через несколько секунд.</p><p class="text-light">Спасибо, уже пошли точить для вас бочонки</p>';
